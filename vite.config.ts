eae9466d975a1022ab25d0663c151df9ee3f2c8f
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2026-05-20 19:14:13 +0900

    chore: add vite-tsconfig-paths for tsconfig alias support

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    tsconfigPaths(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'robots.txt'],
